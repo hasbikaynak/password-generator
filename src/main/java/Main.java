c944024d040d34e58b75d696ba6363bb3fb0fb43
Hasbi Kaynak <kaynakhasbi1@gmail.com>
2022-08-03 22:03:53 +0200

    refactor unused variable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public void start(){
-        Scanner input = new Scanner(System.in);
         System.out.println("Welcome to the Password Generator application\n" +
                 "Please follow the program`s leading");
         Password password = new Password();
